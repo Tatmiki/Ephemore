5b0d8933086a23b608b30b6cdf7b499d91055d2b
Tatmiki <tatmiki@outlook.com>
2025-06-16 21:23:08 -0300

    Primeiro commit - Inicio do projeto

---
+++ src/main/java/com/example/ephemore/EphemoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ephemore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphemoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphemoreApplication.class, args);
+	}
+
+}
